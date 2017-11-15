2a9904dc0998bf1c005e869e21f019b84c0f18a2
mixeych <dmitriy_miheev@mail.ru>
2017-11-15 14:11:22 +0200

    Update welcome.blade.php
    sdfgdsfg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-
+        test
+       
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
